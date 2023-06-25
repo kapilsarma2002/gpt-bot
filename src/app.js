21b85b7b7c944e8e45b91c1dae4465c28ac1d501
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-06-25 12:32:25 +0530

    html

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: 'public' })
+})
+
+app.get('/home', (req, res) => {
+  res.sendFile('home.html', { root: 'public' })
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port number ${port}`)
+})
